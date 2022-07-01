41cf342933a76677c68bbf6d06c975cc8a0a5c09
tarathep <bokie.tarathep@gmail.com>
2022-07-01 10:19:46 +0700

    add comment and ref link at security Conf

--- form-auth/src/main/java/com/tarathep/springsecurityauth/formauth/SecurityConfiguration.java
+++ form-auth/src/main/java/com/tarathep/springsecurityauth/formauth/SecurityConfiguration.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
     
     // use form default
     @Bean
@@ -36,7 +36,8 @@ public class SecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager configure() {
-        
+
+        //password is not work!!
         UserDetails user = User.withUsername("user")
             .password("{bcrypt}$2a$10$DDm1uVxfiZiPmK5amD0ZVu1YdVuLqYHZaw8/NzeuRkZ62/pNLbi/q")
             .roles("USER")
